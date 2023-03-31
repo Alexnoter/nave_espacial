f6d18a38a30c360dc03c40c1966a0488c048dfdc
AlexanderV <alexandervelarde2511@gmail.com>
2023-03-31 19:25:46 -0400

    Descargamos icono , cambiamos el titulo y el icono de pygame y tambien cambieamos el fondo de la pantalla

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 '''
 los eventos son todo lo que ocurra dentro de la pantalla de pygame todo lo que haga
 tenemos que programar eventos
+
+display se refiere a lo que nosotros veremos o mostramos
+
 '''
 import pygame
 
@@ -14,6 +17,11 @@ pygame.init()
 #creamos una pantalla con valores y,x
 pantalla = pygame.display.set_mode((800, 600))
 
+#Editamos el Titulo e Icono del juego
+pygame.display.set_caption("Invasión Espacial")
+icono = pygame.image.load("ovni.png")
+pygame.display.set_icon(icono)
+
 
 #con esto hacemos que la pantalla permanesca abierta y que escuche todos los eventos
 #y se cierre cuando se lo ordenemos
@@ -24,7 +32,11 @@ while se_ejecuta:
         if evento.type == pygame.QUIT:#este evento es el que corresponde a la X de salir
             se_ejecuta = False
 
-##################################################################################3
+    # fill = relleno convinacion de colores RGB(red ,green , blue) tupla ,esta orden necesita que se actualize la pantalla
+    pantalla.fill((205, 144, 228))
+    pygame.display.update()     #actualizamos la pantalla
+
+
 
 
 
